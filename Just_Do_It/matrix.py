cbf4564786edc5d210c3289d60b92712251f9144
Roman Synovets <Roman.Synovets@gmail.com>
2018-07-23 20:42:27 +0300

    Learning python __magic__ methods

---
+++ Just_Do_It/matrix.py
@@ -0,0 +1,17 @@
+matrix_a = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+matrix_b = matrix_a[::-1]
+
+result = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+]
+
+for i in range(len(matrix_a)):
+    for j in range(len(matrix_b[0])):
+        for k in range(len(matrix_b)):
+            pass
